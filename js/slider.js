10b8da11c37bd6c05c592ef144af1769060f3ef3
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-04-15 10:49:46 +0300

    add markup

---
+++ js/slider.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mySwiper = new Swiper(".swiper-container", {
+    // Опції для Swiper
+    initialSlide: 1,
+    slidesPerView: 1, // Кількість видимих слайдів одночасно
+    spaceBetween: 30, // Відстань між слайдами
+    loop: false, // Безкінечна прокрутка
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mySwiper = new Swiper(".swiper-container-c", {
+    // Опції для Swiper
+    initialSlide: 0,
+    slidesPerView: 1, // Кількість видимих слайдів одночасно
+    spaceBetween: 30, // Відстань між слайдами
+    loop: false, // Безкінечна прокрутка
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mySwiper = new Swiper(".swiper-container-s", {
+    // Опції для Swiper
+    initialSlide: 0,
+    slidesPerView: 1, // Кількість видимих слайдів одночасно
+    spaceBetween: 30, // Відстань між слайдами
+    loop: false, // Безкінечна прокрутка
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mySwiper = new Swiper(".swiper-container-i", {
+    // Опції для Swiper
+    initialSlide: 0,
+    slidesPerView: 2, // Кількість видимих слайдів одночасно
+    spaceBetween: 10, // Відстань між слайдами
+    loop: false, // Безкінечна прокрутка
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mySwiper = new Swiper(".swiper-container-u", {
+    // Опції для Swiper
+    initialSlide: 0,
+    slidesPerView: 2, // Кількість видимих слайдів одночасно
+    spaceBetween: 10, // Відстань між слайдами
+    loop: false, // Безкінечна прокрутка
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
